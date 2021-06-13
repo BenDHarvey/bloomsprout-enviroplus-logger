b860aeba01772685d9e6fed0c86284a77688e46d
Ben Harvey <ben@harvey.onl>
2021-06-13 18:41:06 +1000

    * update to try and get the correct time coming through

--- bloomsprout/parse.py
+++ bloomsprout/parse.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 import time
+from datetime import timezone
 
 class BloomsproutParser:
     def parse_enviro_list_to_bloomsprout_object(self, enviro_readings):
-        now = time.time()
+        dt = datetime.datetime.now(timezone.utc)
+
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+
+        #x = utc_timestamp.split(".")
+        x_string = str(utc_timestamp)
+
+        x_split = x_string.split(".")
 
         measurments = []
         for m in enviro_readings:
@@ -12,4 +21,4 @@ class BloomsproutParser:
 
         meta = {"Instrument": "enviro+", "Id": "sense-02"}
 
-        return {"Timestamp": now, "Measurments": measurments, "Meta": meta}
+        return {"Timestamp": x_split[0], "Measurments": measurments, "Meta": meta}
