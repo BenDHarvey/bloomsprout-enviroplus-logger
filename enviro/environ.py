b7568cfdd02b8be36c2d337edddd009ebd3a8eff
BenDHarvey <81058992+BenDHarvey@users.noreply.github.com>
2021-06-18 08:46:52 +1000

    Update environ.py

--- enviro/environ.py
+++ enviro/environ.py
@@ -166,8 +166,7 @@ class Enviro:
             cpu_temps = cpu_temps[1:] + [cpu_temp]
             avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
             raw_temp = self.bme280.get_temperature()
-            #data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
-            data = raw_temp # Just report the raw temp. Planning to move the sensor away from the board so this problem should go away
+            data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
             display_text(con.variables[mode], data, unit)
 
         if mode == 1:
